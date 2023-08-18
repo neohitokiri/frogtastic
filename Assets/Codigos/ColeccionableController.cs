bedbae0979aedd9950dd3832019a11af37ae018b
Javier Leonardo Cerón Puentes <javierleonardoceron@gmail.com>
2023-08-18 12:36:40 -0500

    Organiza assets, cambia bombillos

--- Assets/Codigos/ColeccionableController.cs
+++ Assets/Codigos/ColeccionableController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
+public enum TipoDeColeccionable
+{
+    Vela = 0,
+    Bombillo = 1,
+}
+
 public class ColeccionableController : MonoBehaviour
 {
     [SerializeField] private bool collected;
@@ -11,11 +17,18 @@ public class ColeccionableController : MonoBehaviour
     [SerializeField] public Light2D luzCentral;
     [SerializeField] public Light2D luzProyeccion;
     [SerializeField] public AudioSource audioSource;
+    [SerializeField] public TipoDeColeccionable Tipo;
+    [SerializeField] private SpriteRenderer sprite;
 
+    [SerializeField] private Texture2D ImagenAntesDeTomar;
+    [SerializeField] private Texture2D ImagenDespuesDeTomar;
 
-    private void Start() {
+//    private void Awake() {
+//    }
+
+    private void Awake() {
         collected = false;
-//        light2D = GetComponent<Light2D>();
+        sprite = GetComponent<SpriteRenderer>();
         foreach(Transform child in transform)
         {
             if (child.gameObject.name == "LuzCentral")
@@ -26,6 +39,34 @@ public class ColeccionableController : MonoBehaviour
                 audioSource = child.gameObject.GetComponent<AudioSource>();
         }
 
+        string _antes = "";
+        string _despues = "";
+
+        switch(Tipo)
+        {
+            case TipoDeColeccionable.Vela:
+                _antes = "Arte/Propio/bombillo-amarillo";
+                _despues = "Arte/Propio/bombillo-blanco";
+                break;
+            case TipoDeColeccionable.Bombillo:
+                _antes = "Arte/Propio/bombillo-amarillo";
+                _despues = "Arte/Propio/bombillo-blanco";
+                break;
+        }
+
+        // Configura textura
+        ImagenAntesDeTomar = Resources.Load<Texture2D>(_antes);
+        ImagenDespuesDeTomar = Resources.Load<Texture2D>(_despues);
+        sprite.sprite = CrearSpriteDeTextura(ImagenAntesDeTomar);
+    }
+
+    private Sprite CrearSpriteDeTextura(Texture2D Textura)
+    {
+        return Sprite.Create(
+            Textura, 
+            new Rect(0, 0, Textura.width, Textura.height),
+            new Vector2(0.5f, 0.5f)
+        );
     }
 
     // Start is called before the first frame update
@@ -36,10 +77,12 @@ public class ColeccionableController : MonoBehaviour
                 puntaje.SumarPuntos(1.0f);
 //                Destroy(gameObject);
 //                other.GetComponent<MovimientoPersonaje>().Iluminar();
-                luzCentral.intensity++;
-                luzProyeccion.intensity++;
+                luzCentral.intensity+=0.2f;
+                luzProyeccion.intensity+=0.6f;
+                luzProyeccion.color = Color.white;
                 audioSource.Play();
                 collected = true;
+                sprite.sprite = CrearSpriteDeTextura(ImagenDespuesDeTomar);
             }
         }
     }
